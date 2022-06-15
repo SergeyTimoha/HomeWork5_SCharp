e5410036960ed189a7e691699c48e092ecda5ad8
Sergey <serega79ser@mail.ru>
2022-06-15 23:58:24 +0600

    Задача 34

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+Console.Clear();
+int[] numbers = FillArray(10, 10, 999);
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine($"Число чётных элементов в массиве: {EvenNumbers(numbers)}.");
+
+int[] FillArray(int size, int mixValue, int maxValue)
+{
+    int[] getArray = new int[size].Select(x => new Random().Next(mixValue, maxValue + 1)).ToArray();
+    return getArray;
+}
+
+int EvenNumbers(int[] numArray)
+{
+    int counter = numArray.Where(x => x % 2 == 0).Count();
+    return counter;
+}
