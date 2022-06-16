1ac5fee01dc327b455ef24e0e1c397fb607577f2
Sergey <serega79ser@mail.ru>
2022-06-16 19:51:54 +0600

    Задача 36

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+Console.Clear();
+int[] numbers = FillArray(10, 0, 99);
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine($"Сумма элементов с нечётным индексом: {sumOddNum(numbers)}.");
+
+int[] FillArray(int size, int mixValue, int maxValue)
+{
+    int[] getArray = new int[size].Select(x => new Random().Next(mixValue, maxValue + 1)).ToArray();
+    return getArray;
+}
+
+int sumOddNum(int[] numArray)
+{
+    int sum = 0;
+    for (int i = 0; i < numArray.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += numArray[i];
+        }
+    }
+    return sum;
+}
