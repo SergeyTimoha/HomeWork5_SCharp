30d56d8157365ec83e1234a63ece19c8fa8c1fd5
Sergey <serega79ser@mail.ru>
2022-06-16 20:00:30 +0600

    Задача 38

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+Console.Clear();
+int[] numbers = FillArray(10, 0, 99);
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine($"Разность максимального и минимального элементов массива: {Max(numbers) - Min(numbers)}.");
+
+int[] FillArray(int size, int mixValue, int maxValue)
+{
+    int[] getArray = new int[size].Select(x => new Random().Next(mixValue, maxValue + 1)).ToArray();
+    return getArray;
+}
+
+int Min(int[] numArr)
+{
+    int min = numArr.Min();
+    return min;
+}
+
+int Max(int[] numArray)
+{
+    int max = numArray.Max();
+    return max;
+}
